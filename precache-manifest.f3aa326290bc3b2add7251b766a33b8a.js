6f2a68b8d63102ee73e3008d16a2020604d97035
NovikovRomanV <rimm19845@gmail.com>
2020-05-22 23:55:20 +0300

    Updates

---
+++ precache-manifest.f3aa326290bc3b2add7251b766a33b8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "126992207e6f450ef836ef46680cf13a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "460906c423e1e370950b",
+    "url": "/portfolio/static/css/main.8b8be81c.chunk.css"
+  },
+  {
+    "revision": "f13cd9cf12d28c7eef76",
+    "url": "/portfolio/static/js/2.edc00463.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.edc00463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "460906c423e1e370950b",
+    "url": "/portfolio/static/js/main.f20fe1cf.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  }
+]);
